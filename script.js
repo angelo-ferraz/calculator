d5b795c6bd1ba4dfcd127cc7b10ed0f33997691d
angelo-ferraz <104265131+angelo-ferraz@users.noreply.github.com>
2022-05-31 14:11:26 +0100

    Added option to string together calcs When pressing multiple operators after another, it performs an auto calc

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// To Do: make operator unselectable without number
+//         
+
+
+
 const buttons = document.querySelectorAll('button')
 const operators = document.querySelectorAll('.operator')
 const janitors = document.querySelectorAll('.janitor')
@@ -5,22 +10,33 @@ const previousDisplay = document.querySelector('[data-previous]')
 const currentDisplay = document.querySelector('[data-current]')
 let firstNumber
 let secondNumber
-let result
+let result = '0'
 let operator
+let calcDone = false
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const value = button.textContent
         const previous = previousDisplay.textContent
         const current = currentDisplay.textContent
+        if (currentDisplay.textContent.includes(undefined)){currentDisplay.textContent = '0'}
+        if (currentDisplay.textContent.includes(NaN)){currentDisplay.textContent = '0'}
+        if (previousDisplay.textContent.includes(undefined)){currentDisplay.textContent = ''}
+        if (previousDisplay.textContent.includes(NaN)){currentDisplay.textContent = ''}
+
 
         // Screen Functionality
         if (button.classList.contains('number')) {
-            if (current === '0') {
+            if (calcDone === true){
+                previousDisplay.textContent = ``
+                currentDisplay.textContent = button.textContent}
+            else if (current === '0') {
                 currentDisplay.textContent = value} 
-            else {currentDisplay.textContent = current + value}}
+            else {currentDisplay.textContent = current + value}
+        calcDone = false}
         
         if (button.classList.contains('decimal')) {
+            calcDone = false
             // Stopping empty .
             if (current === '') {
                 currentDisplay.textContent = '0.'}
@@ -30,35 +46,102 @@ buttons.forEach(button => {
             else {currentDisplay.textContent = current + '.'}}
 
         if (button.classList.contains('operator')) {
-            // Prevent multiple operator clicks
-            firstNumber = parseFloat(current)
+            if (currentDisplay.textContent === '0'){return}
+            if (previousDisplay.textContent.includes(operator) && calcDone === false){
+                switch  (operator){
+                    case '+':
+                        operator = button.textContent
+                        secondNumber = parseFloat(current)
+                        firstNumber = parseFloat(previousDisplay.innerText.slice(0, -2))
+                        if (String(firstNumber).includes('.') || String(secondNumber).includes('.')){
+                            result = add(firstNumber, secondNumber).toFixed(2)}
+                        else result = add(firstNumber, secondNumber)  
+                        previousDisplay.textContent = `${result} ${operator}` 
+                        currentDisplay.textContent = ''
+                        break
+                    case '-':
+                        operator = button.textContent
+                        secondNumber = parseFloat(current)
+                        firstNumber = parseFloat(previousDisplay.innerText.slice(0, -2))
+                        if (String(firstNumber).includes('.') || String(secondNumber).includes('.')){
+                            result = subtract(firstNumber, secondNumber).toFixed(2)}
+                        else result = subtract(firstNumber, secondNumber)  
+                        previousDisplay.textContent = `${result} ${operator}` 
+                        currentDisplay.textContent = ''
+                        break
+                    case '*':
+                        operator = button.textContent
+                        secondNumber = parseFloat(current)
+                        firstNumber = parseFloat(previousDisplay.innerText.slice(0, -2))
+                        if (String(firstNumber).includes('.') || String(secondNumber).includes('.')){
+                            result = multiply(firstNumber, secondNumber).toFixed(2)}
+                        else result = multiply(firstNumber, secondNumber)  
+                        previousDisplay.textContent = `${result} ${operator}` 
+                        currentDisplay.textContent = ''
+                        break
+                    case '÷':
+                        operator = button.textContent
+                        secondNumber = parseFloat(current)
+                        firstNumber = parseFloat(previousDisplay.innerText.slice(0, -2))
+                        if (String(firstNumber).includes('.') || String(secondNumber).includes('.')){
+                            result = divide(firstNumber, secondNumber).toFixed(2)}
+                        else result = divide(firstNumber, secondNumber)  
+                        previousDisplay.textContent = `${result} ${operator}` 
+                        currentDisplay.textContent = ''
+                        break
+                    default:
+                        break}
+            }
+            
+            else {
+            calcDone = false
             operator = button.textContent
+            firstNumber = parseFloat(current)
             operators.forEach(selected => {selected.dataset.state = ''})
             button.dataset.state = 'selected'
             previousDisplay.textContent = current + ' ' + operator
-            currentDisplay.textContent = ''}
+            currentDisplay.textContent = ''}}
         
         if (button.classList.contains('equals')) {
+            secondNumber = parseFloat(current)
+            firstNumber = parseFloat(previousDisplay.innerText.slice(0, -2))
+
+            if (currentDisplay.textContent === '' || previousDisplay.textContent !== firstNumber + ' ' + operator){
+                previousDisplay.textContent = 'Error'
+                operators.forEach(selected => {selected.dataset.state = ''})
+                return
+            }
+
             operators.forEach(selected => {selected.dataset.state = ''})
             previousDisplay.textContent = `${previous} ${current} =`
 
             // Calculation
-            secondNumber = parseFloat(current)
 
             switch (operator) {
                 case '+':
-                    result = add(firstNumber, secondNumber)
+                    if (String(firstNumber).includes('.') || String(secondNumber).includes('.')){
+                        result = add(firstNumber, secondNumber).toFixed(2)}
+                    else result = add(firstNumber, secondNumber)
                     break
                 case '-':
-                    result = subtract(firstNumber, secondNumber)
+                    if (String(firstNumber).includes('.') || String(secondNumber).includes('.')){
+                        result = subtract(firstNumber, secondNumber).toFixed(2)}
+                    else result = subtract(firstNumber, secondNumber)
                     break
                 case '*':
-                    result = multiply(firstNumber, secondNumber)
+                if (String(firstNumber).includes('.') || String(secondNumber).includes('.')){
+                        result = multiply(firstNumber, secondNumber).toFixed(2)}
+                    else result = multiply(firstNumber, secondNumber)
                     break
                 case '÷':
-                    result = divide(firstNumber, secondNumber)
+                    if (String(firstNumber).includes('.') || String(secondNumber).includes('.')){
+                        result = divide(firstNumber, secondNumber).toFixed(2)}
+                    else result = divide(firstNumber, secondNumber)
+                    break
+                default:
                     break}
 
+            calcDone = true
             currentDisplay.textContent = `${result}`
             console.log(firstNumber, secondNumber)}
         
@@ -70,9 +153,7 @@ buttons.forEach(button => {
                     operators.forEach(selected => {selected.dataset.state = ''})
                     break
                 case 'DEL':
-                    console.log(currentDisplay);
                     currentDisplay.textContent = currentDisplay.innerText.slice(0, -1)
-                    operators.forEach(selected => {selected.dataset.state = ''})
                     break
                 case 'ANS':
                     currentDisplay.textContent = `${result}`
@@ -92,3 +173,4 @@ function multiply(firstNumber, secondNumber){
 
 function divide(firstNumber, secondNumber){
     return firstNumber / secondNumber}
+
